58b7efa2a30db8c1418323bd487227d978fd2795
wilson <qwe789qwec@gmail.com>
2024-12-02 21:09:58 +0900

    modify game name

--- tool/arrange_game_name.py
+++ tool/arrange_game_name.py
@@ -4,6 +4,7 @@ import random
 import time
 import cv2
 import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from triple_town_game import playgame
 
@@ -22,18 +23,21 @@ for image_file in image_files:
     else:
         continue
     
-    game_info = image_file.replace("game_", "").replace(".png", "")
-    game_info = game_info.split("_")
+    part_before_info = image_file.split("_info_")[0]
+    part_before_info = part_before_info.replace("game_", "")
+    game_info = part_before_info.split("_")
     num, step, action = map(int, game_info)
     action_str = str(action) + ".png"
     num = num
     step = step
     new_name = "_".join(["game", str(num), str(step), action_str])
 
-    part_before_info = image_file.split("_info_")[0]
-    new_name = part_before_info + ".png"
+    part_after_info = image_file.split("_info_")[1]
+    part_after_info = part_after_info.replace(".png", "")
+
+    # new_name = "game_" + part_before_info + ".png"
     print(new_name)
     new_path = os.path.join(game_folder, new_name)
     image_path = os.path.join(game_folder, image_file)
-    # os.rename(image_path, new_path)
-    break
+    os.rename(image_path, new_path)
+    # break
