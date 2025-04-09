7da7e0d46ed5f655b4056494415a244ff615ad16
qwe789qwec <qwe789qwec@gmail.com>
2025-04-09 15:48:03 +0900

    modify path

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 TRAIN = False
-VALIDATE = False
-REALPLAY = True
+VALIDATE = True
+REALPLAY = False
 
 def main():
     # 初始化環境和模型
     env = TripleTownSim()
     net = TripleTownNet(device, board_size=6, num_piece_types=22)
+    model_path = "models/triple_town_model_ep400.pt"
     
     # 訓練智能體
     agent = TripleTownAgent(net, device, env)
-    if os.path.exists("models/triple_town_model_ep300.pt"):
-        agent.load("models/triple_town_model_ep300.pt")
+    if os.path.exists(model_path):
+        agent.load(model_path)
 
     if TRAIN:
-        agent.train(episodes=1000, MCTS_depth=1000)
+        agent.train(episodes=1000, MCTS_depth=300)
 
         # 保存最終模型
         torch.save(net.state_dict(), "triple_town_model_final.pt")
